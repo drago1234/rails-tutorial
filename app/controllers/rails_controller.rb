c682a786de5106db1c62e0b90bfac485c8dc1245
Drago1234 <dong.760@osu.edu>
2019-07-12 06:32:19 +0000

    Add a Static Pages controllers

---
+++ app/controllers/rails_controller.rb
@@ -0,0 +1,16 @@
+class RailsController < ApplicationController
+  def generate
+  end
+
+  def controller
+  end
+
+  def StaticPages
+  end
+
+  def home
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'rails/generate'
+  get 'rails/controller'
+  get 'rails/StaticPages'
+  get 'rails/home'
+  get 'rails/help'
   root 'application#hello'
 end
