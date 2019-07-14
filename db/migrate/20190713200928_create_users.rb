39b69b66e8fba752f7ff0855fe97ad271a5e585d
Drago1234 <dong.760@osu.edu>
2019-07-14 14:27:36 +0000

    Make a basic User model (including secure passwords)

---
+++ db/migrate/20190713200928_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+    #The above code would create 2 table
+  end
+end
